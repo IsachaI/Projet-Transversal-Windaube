0370792ec1e516f8644c5591986a3971f6f67782
Louis <louisdegand2007lol@gmail.com>
2026-02-13 17:28:49 +0100

    Update led.py

--- led.py
+++ led.py
@@ -2,12 +2,20 @@ from gpiozero import LED
 from time import sleep
 green = LED(17)
 red = LED(22)
+blue = LED(5)
 def allumer():
-    green.on
-    sleep(1)
-    green.off
-    sleep(1)
-    red.on
-    sleep(1)
-    red.off
-    sleep(1)
+    green.on()
+    sleep(0.2)
+    green.off()
+    sleep(0.2)
+    red.on()
+    sleep(0.2)
+    red.off()
+    sleep(0.2)
+    blue.on()
+    sleep(0.2)
+    blue.off()
+    sleep(0.2)
+
+while True:
+    allumer()
