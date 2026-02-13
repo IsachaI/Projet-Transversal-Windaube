1149dd4b086d6989d065c213a00906a3fe277708
Valentin Thiel <valentin.thiel@student.uclouvain.be>
2026-02-13 16:00:55 +0100

    script avec deux led pin 17 et pin22

--- led.py
+++ led.py
@@ -1,8 +1,13 @@
 from gpiozero import LED
 from time import sleep
 green = LED(17)
+red = LED(22)
 def allumer():
     green.on
     sleep(1)
     green.off
+    sleep(1)
+    red.on
+    sleep(1)
+    red.off
     sleep(1)
